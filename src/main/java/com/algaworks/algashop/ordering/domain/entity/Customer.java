a951603a75cd7753a40f228ef7fc0552123565c9
henrick.nogueira <henrick.nogueira@cnpq.br>
2025-07-02 13:55:13 -0300

    5.9. Refinando Domain Model

---
+++ src/main/java/com/algaworks/algashop/ordering/domain/entity/Customer.java
@@ -0,0 +1,19 @@
+package com.algaworks.algashop.ordering.domain.entity;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public class Customer {
+    private UUID id;
+    private String fullName;
+    private LocalDate birthDate;
+    private String email;
+    private String phone;
+    private String document;
+    private Boolean promotionNotificationsAllowed;
+    private Boolean archived;
+    private OffsetDateTime registeredAt;
+    private OffsetDateTime archivedAt;
+    private Integer loyaltyPoints;
+}
