3bbb18a23d7957bf6e757446fa14f7ee93952b32
henrick.nogueira <henrick.nogueira@cnpq.br>
2025-07-04 21:27:33 -0300

    5.13. Refatorando a Customer Entity em direção a um Rich model

--- src/main/java/com/algaworks/algashop/ordering/domain/entity/Customer.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/entity/Customer.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
@@ -23,4 +24,161 @@ public class Customer {
     private OffsetDateTime registeredAt;
     private OffsetDateTime archivedAt;
     private Integer loyaltyPoints;
+
+    public Customer(UUID id, String fullName, LocalDate birthDate, String email, String phone, String document,
+                    Boolean promotionNotificationsAllowed, OffsetDateTime registeredAt) {
+        this.id = id;
+        this.fullName = fullName;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.phone = phone;
+        this.document = document;
+        this.promotionNotificationsAllowed = promotionNotificationsAllowed;
+        this.registeredAt = registeredAt;
+    }
+
+    public Customer(UUID id, String fullName, LocalDate birthDate, String email, String phone, String document,
+                    Boolean promotionNotificationsAllowed, Boolean archived, OffsetDateTime registeredAt,
+                    OffsetDateTime archivedAt, Integer loyaltyPoints) {
+        this.id = id;
+        this.fullName = fullName;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.phone = phone;
+        this.document = document;
+        this.promotionNotificationsAllowed = promotionNotificationsAllowed;
+        this.archived = archived;
+        this.registeredAt = registeredAt;
+        this.archivedAt = archivedAt;
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    public void addLoyaltyPoints(Integer points) {
+
+    }
+
+    public void archive() {
+
+    }
+
+    public void enablePromotionNotifications() {
+        this.setPromotionNotificationsAllowed(true);
+    }
+
+    public void disablePromotionNotifications() {
+        this.setPromotionNotificationsAllowed(false);
+    }
+
+    public void changeName(String fullName) {
+        this.setFullName(fullName);
+    }
+
+    public void changeEmail(String email) {
+        this.setEmail(email);
+    }
+
+    public void changePhone(String phone) {
+        this.setPhone(phone);
+    }
+
+    private UUID getId() {
+        return id;
+    }
+
+    private void setId(UUID id) {
+        this.id = id;
+    }
+
+    private String getFullName() {
+        return fullName;
+    }
+
+    private void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    private LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    private void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    private String getEmail() {
+        return email;
+    }
+
+    private void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String getPhone() {
+        return phone;
+    }
+
+    private void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    private String getDocument() {
+        return document;
+    }
+
+    private void setDocument(String document) {
+        this.document = document;
+    }
+
+    private Boolean getPromotionNotificationsAllowed() {
+        return promotionNotificationsAllowed;
+    }
+
+    private void setPromotionNotificationsAllowed(Boolean promotionNotificationsAllowed) {
+        this.promotionNotificationsAllowed = promotionNotificationsAllowed;
+    }
+
+    private Boolean getArchived() {
+        return archived;
+    }
+
+    private void setArchived(Boolean archived) {
+        this.archived = archived;
+    }
+
+    private OffsetDateTime getRegisteredAt() {
+        return registeredAt;
+    }
+
+    private void setRegisteredAt(OffsetDateTime registeredAt) {
+        this.registeredAt = registeredAt;
+    }
+
+    private OffsetDateTime getArchivedAt() {
+        return archivedAt;
+    }
+
+    private void setArchivedAt(OffsetDateTime archivedAt) {
+        this.archivedAt = archivedAt;
+    }
+
+    private Integer getLoyaltyPoints() {
+        return loyaltyPoints;
+    }
+
+    private void setLoyaltyPoints(Integer loyaltyPoints) {
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) && Objects.equals(fullName, customer.fullName) && Objects.equals(birthDate, customer.birthDate) && Objects.equals(email, customer.email) && Objects.equals(phone, customer.phone) && Objects.equals(document, customer.document) && Objects.equals(promotionNotificationsAllowed, customer.promotionNotificationsAllowed) && Objects.equals(archived, customer.archived) && Objects.equals(registeredAt, customer.registeredAt) && Objects.equals(archivedAt, customer.archivedAt) && Objects.equals(loyaltyPoints, customer.loyaltyPoints);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fullName, birthDate, email, phone, document, promotionNotificationsAllowed, archived, registeredAt, archivedAt, loyaltyPoints);
+    }
 }
--- src/test/java/com/algaworks/algashop/ordering/domain/entity/CustomerTest.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/entity/CustomerTest.java
@@ -2,16 +2,26 @@ package com.algaworks.algashop.ordering.domain.entity;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
 public class CustomerTest {
 
     @Test
     public void testingCustomer() {
-        Customer customer = new Customer();
-        customer.setId(null);
-        customer.setFullName("Henrick Nogueira");
-        customer.setDocument(null);
-        customer.setLoyaltyPoints(10);
-        customer.getId();
+        Customer customer = new Customer(
+                UUID.randomUUID(),
+                "John Doe",
+                LocalDate.of(1991, 7, 5),
+                "john@email.com",
+                "123-123-1231",
+                "255-08-0578",
+                true,
+                OffsetDateTime.now()
+        );
+
+        customer.addLoyaltyPoints(10);
     }
 
 }
