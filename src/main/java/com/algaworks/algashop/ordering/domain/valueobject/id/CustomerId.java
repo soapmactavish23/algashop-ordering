88336d3615ac3346b9b113e1c3ab92782785cc7d
henrick.nogueira <henrick.nogueira@cnpq.br>
2025-07-31 14:43:11 -0300

    6.13. Implementando novos identificadores

---
+++ src/main/java/com/algaworks/algashop/ordering/domain/valueobject/id/CustomerId.java
@@ -0,0 +1,24 @@
+package com.algaworks.algashop.ordering.domain.valueobject.id;
+
+import com.algaworks.algashop.ordering.domain.utility.IdGenerator;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public record CustomerId(UUID value) {
+
+    public CustomerId() {
+        this(IdGenerator.generateTimeBasedUUID());
+    }
+
+    public CustomerId(UUID value) {
+        Objects.requireNonNull(value);
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
+}
---
+++ src/main/java/com/algaworks/algashop/ordering/domain/valueobject/id/ProductId.java
@@ -0,0 +1,24 @@
+package com.algaworks.algashop.ordering.domain.valueobject.id;
+
+import com.algaworks.algashop.ordering.domain.utility.IdGenerator;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public record ProductId(UUID value) {
+
+    public ProductId() {
+        this(IdGenerator.generateTimeBasedUUID());
+    }
+
+    public ProductId(UUID value) {
+        Objects.requireNonNull(value);
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
+}
