c5a0f8a84c5cd8ee72c29b4ca56d9909dc3582fd
henrick.nogueira <ricknogueira1231@gmail.com>
2025-09-07 16:14:55 -0300

    6.22. Implementando regras de negócio para garantir invariantes

--- src/main/java/com/algaworks/algashop/ordering/domain/entity/Order.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/entity/Order.java
@@ -1,5 +1,6 @@
 package com.algaworks.algashop.ordering.domain.entity;
 
+import com.algaworks.algashop.ordering.domain.exceptions.OrderCannotBePlacedException;
 import com.algaworks.algashop.ordering.domain.exceptions.OrderInvalidShippingDeliveryDateException;
 import com.algaworks.algashop.ordering.domain.exceptions.OrderStatusCannotBeChangedException;
 import com.algaworks.algashop.ordering.domain.valueobject.*;
@@ -105,7 +106,18 @@ public class Order {
     }
 
     public void place() {
-        //TODO: Business rules
+        Objects.requireNonNull(this.shipping());
+        Objects.requireNonNull(this.billing());
+        Objects.requireNonNull(this.expectedDeliveryDate());
+        Objects.requireNonNull(this.shippingCost());
+        Objects.requireNonNull(this.paymentMethod());
+        Objects.requireNonNull(this.items());
+
+        if(this.items().isEmpty()) {
+            throw new OrderCannotBePlacedException(this.id());
+        }
+
+        this.setPlacedAt(OffsetDateTime.now());
         this.changeStatus(OrderStatus.PLACED);
     }
 
---
+++ src/main/java/com/algaworks/algashop/ordering/domain/exceptions/OrderCannotBePlacedException.java
@@ -0,0 +1,11 @@
+package com.algaworks.algashop.ordering.domain.exceptions;
+
+import com.algaworks.algashop.ordering.domain.valueobject.id.OrderId;
+
+public class OrderCannotBePlacedException extends DomainException {
+
+    public OrderCannotBePlacedException(OrderId id) {
+        super(String.format(ErrorMessages.ERROR_ORDER_CANNOT_BE_PLACED_HAS_NOT_ITEMS, id));
+    }
+
+}
