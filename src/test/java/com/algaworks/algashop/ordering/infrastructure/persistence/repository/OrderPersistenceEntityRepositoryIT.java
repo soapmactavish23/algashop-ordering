c4f027cc5f22363b407dbad83fb6c7e4e394c342
henrick.nogueira <ricknogueira1231@gmail.com>
2025-09-23 22:49:17 -0300

    8.8. Isolando execução dos testes de integração

---
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -0,0 +1,16 @@
+package com.algaworks.algashop.ordering.infrastructure.persistence.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class OrderPersistenceEntityRepositoryIT {
+
+    @Test
+    public void shouldRun() {
+
+    }
+
+}
