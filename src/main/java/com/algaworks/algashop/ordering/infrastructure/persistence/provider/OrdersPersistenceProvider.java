b2bfed10ee5d406454944f5dd41dc9024d3233cb
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-03 17:51:12 -0300

    8.20. Protegendo alterações indevidas de Version

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/provider/OrdersPersistenceProvider.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/provider/OrdersPersistenceProvider.java
@@ -9,8 +9,11 @@ import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPe
 import com.algaworks.algashop.ordering.infrastructure.persistence.repository.OrderPersistenceEntityRepository;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.Optional;
 
 @Component
@@ -55,13 +58,22 @@ public class OrdersPersistenceProvider implements Orders {
         persistenceEntity = assembler.merge(persistenceEntity, aggregateRoot);
         entityManager.detach(persistenceEntity);
         persistenceEntity = persistenceRepository.saveAndFlush(persistenceEntity);
-        aggregateRoot.setVersion(persistenceEntity.getVersion());
+        updateVersion(aggregateRoot, persistenceEntity);
     }
+
 
     private void insert(Order aggregateRoot) {
         OrderPersistenceEntity persistenceEntity = assembler.fromDomain(aggregateRoot);
         persistenceRepository.saveAndFlush(persistenceEntity);
-        aggregateRoot.setVersion(persistenceEntity.getVersion());
+        updateVersion(aggregateRoot, persistenceEntity);
+    }
+
+    @SneakyThrows
+    private void updateVersion(Order aggregateRoot, OrderPersistenceEntity persistenceEntity) {
+        Field version = aggregateRoot.getClass().getDeclaredField("version");
+        version.setAccessible(true);
+        ReflectionUtils.setField(version, aggregateRoot, persistenceEntity.getVersion());
+        version.setAccessible(false);
     }
 
     @Override
