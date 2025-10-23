1fdfbea2ac700f7ea2801e11916cbc1ca6dabc6b
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-23 14:35:53 -0300

    9.4. Desenvolvendo o Domain Service para registro de Customer

---
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/service/CustomerRegistrationService.java
@@ -0,0 +1,44 @@
+package com.algaworks.algashop.ordering.domain.model.service;
+
+import com.algaworks.algashop.ordering.domain.model.entity.Customer;
+import com.algaworks.algashop.ordering.domain.model.exceptions.CustomerEmailIsUseException;
+import com.algaworks.algashop.ordering.domain.model.repository.Customers;
+import com.algaworks.algashop.ordering.domain.model.valueobject.*;
+import com.algaworks.algashop.ordering.domain.model.valueobject.id.CustomerId;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CustomerRegistrationService {
+
+    private Customers customers;
+
+    public Customer register(FullName fullName, BirthDate birthDate, Email email,
+                             Phone phone, Document document, Boolean promotionNotificationsAllowed,
+                             Address address) {
+        Customer customer = Customer.brandNew()
+                .fullName(fullName)
+                .birthDate(birthDate)
+                .email(email)
+                .phone(phone)
+                .document(document)
+                .promotionNotificationsAllowed(promotionNotificationsAllowed)
+                .address(address)
+                .build();
+
+        verifyEmailUniqueness(customer.email(), customer.id());
+
+        return customer;
+    }
+
+    public void changeEmail(Customer customer, Email newEmail) {
+        verifyEmailUniqueness(newEmail, customer.id());
+        customer.changeEmail(newEmail);
+    }
+
+    private void verifyEmailUniqueness(Email email, CustomerId customerId) {
+        if (!customers.isEmailUnique(email, customerId)) {
+            throw new CustomerEmailIsUseException();
+        }
+    }
+
+}
