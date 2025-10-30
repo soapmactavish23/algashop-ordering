82777f3344f627b6011e3544fd5a5946df13aac3
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-29 21:17:00 -0300

    10.5. Integração com Spring RestClient

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/client/rapidex/ShippingCostServiceRapidexImpl.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/client/rapidex/ShippingCostServiceRapidexImpl.java
@@ -3,12 +3,14 @@ package com.algaworks.algashop.ordering.infrastructure.client.rapidex;
 import com.algaworks.algashop.ordering.domain.model.service.ShippingCostService;
 import com.algaworks.algashop.ordering.domain.model.valueobject.Money;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "algashop.integrations.shipping.provider", havingValue = "RAPIDEX")
 public class ShippingCostServiceRapidexImpl implements ShippingCostService {
 
     private final RapiDexAPIClient rapiDexAPIClient;
--- src/main/java/com/algaworks/algashop/ordering/infrastructure/fake/ShippingCostFakeImpl.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/fake/ShippingCostFakeImpl.java
@@ -2,9 +2,11 @@ package com.algaworks.algashop.ordering.infrastructure.fake;
 
 import com.algaworks.algashop.ordering.domain.model.service.ShippingCostService;
 import com.algaworks.algashop.ordering.domain.model.valueobject.Money;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 import java.time.LocalDate;
 
+@ConditionalOnProperty(name = "algashop.integrations.shipping.provider", havingValue = "FAKE")
 public class ShippingCostFakeImpl implements ShippingCostService {
     @Override
     public CalculationResult calculate(CalculationRequest request) {
