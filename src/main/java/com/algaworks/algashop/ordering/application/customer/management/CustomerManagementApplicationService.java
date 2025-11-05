665ca1ea460ee5a6bc1f0db6a06d39a9456cad1f
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-04 23:57:19 -0300

    12.5. Mapeadores automáticos com ModelMapper

--- src/main/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationService.java
+++ src/main/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationService.java
@@ -1,6 +1,7 @@
 package com.algaworks.algashop.ordering.application.customer.management;
 
 import com.algaworks.algashop.ordering.application.commons.AddressData;
+import com.algaworks.algashop.ordering.application.utility.Mapper;
 import com.algaworks.algashop.ordering.domain.model.commons.*;
 import com.algaworks.algashop.ordering.domain.model.customer.*;
 import lombok.AllArgsConstructor;
@@ -16,6 +17,7 @@ public class CustomerManagementApplicationService {
 
     private final CustomerRegistrationService customerRegistration;
     private final Customers customers;
+    private final Mapper mapper;
 
     @Transactional
     public UUID create(CustomerInput input) {
@@ -48,31 +50,11 @@ public class CustomerManagementApplicationService {
     @Transactional(readOnly = true)
     public CustomerOutput findById(UUID customerId) {
         Objects.requireNonNull(customerId);
-        Customer customer = customers.ofId(new CustomerId(customerId))
-                .orElseThrow(CustomerNotFoundException::new);
-        return CustomerOutput.builder()
-                .id(customer.id().value())
-                .firstName(customer.fullName().firstName())
-                .lastName(customer.fullName().lastName())
-                .email(customer.email().value())
-                .document(customer.document().value())
-                .phone(customer.phone().value())
-                .promotionNotificationsAllowed(customer.isPromotionNotificationsAllowed())
-                .loyaltyPoints(customer.loyaltyPoints().value())
-                .registeredAt(customer.registeredAt())
-                .archived(customer.isArchived())
-                .archivedAt(customer.archivedAt() != null ? customer.archivedAt() : null)
-                .birthDate(customer.birthDate() != null ? customer.birthDate().value() : null)
-                .address(AddressData.builder()
-                        .street(customer.address().street())
-                        .number(customer.address().number())
-                        .complement(customer.address().complement())
-                        .neighborhood(customer.address().neighborhood())
-                        .city(customer.address().city())
-                        .state(customer.address().state())
-                        .zipCode(customer.address().zipCode().value())
-                        .build())
-                .build();
+        Customer customer = customers.ofId(new CustomerId(customerId)).orElseThrow(CustomerNotFoundException::new);
+
+
+
+        return mapper.convert(customer, CustomerOutput.class);
     }
 
 }
