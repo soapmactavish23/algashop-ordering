3ac60e40790519376bce4dc1b9448c64aee698f8
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-26 18:24:36 -0300

    15.5. Modelo de leitura detalhado para Order

---
+++ src/main/java/com/algaworks/algashop/ordering/application/order/query/OrderItemDetailOutput.java
@@ -0,0 +1,37 @@
+package com.algaworks.algashop.ordering.application.order.query;
+
+import com.algaworks.algashop.ordering.application.checkout.BillingData;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemDetailOutput {
+    private String id;
+    private String orderId;
+    private UUID productId;
+    private String productName;
+    private BigDecimal price;
+    private Integer quantity;
+    private BigDecimal totalAmount;
+    private OffsetDateTime placetAt;
+    private OffsetDateTime paidAt;
+    private OffsetDateTime canceledAt;
+    private OffsetDateTime readyAt;
+    private String status;
+    private String paymentMethod;
+    private ShippingData shipping;
+    private BillingData billing;
+
+    private List<OrderItemDetailOutput> items = new ArrayList<>();
+}
