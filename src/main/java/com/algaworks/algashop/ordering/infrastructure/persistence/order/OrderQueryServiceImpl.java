41d9e0c3c21514c3db86dcde5ce5a4fc31d53303
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-27 20:41:59 -0300

    15.6. Implementando consulta detalhada para Order

---
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/order/OrderQueryServiceImpl.java
@@ -0,0 +1,26 @@
+package com.algaworks.algashop.ordering.infrastructure.persistence.order;
+
+import com.algaworks.algashop.ordering.application.order.query.OrderDetailOutput;
+import com.algaworks.algashop.ordering.application.order.query.OrderQueryService;
+import com.algaworks.algashop.ordering.application.utility.Mapper;
+import com.algaworks.algashop.ordering.domain.model.order.OrderId;
+import com.algaworks.algashop.ordering.domain.model.order.OrderNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderQueryServiceImpl implements OrderQueryService {
+
+    private final OrderPersistenceEntityRepository repository;
+    private final Mapper mapper;
+
+    @Override
+    public OrderDetailOutput findById(String id) {
+        OrderPersistenceEntity entity = repository.findById(new OrderId(id).value().toLong())
+                .orElseThrow(OrderNotFoundException::new);
+        return mapper.convert(entity, OrderDetailOutput.class);
+    }
+}
