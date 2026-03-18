7d6707ecc9061232ca7cab9b5b0bbf91886c797a
soapmactavish23 <ricknogueira1231@gmail.com>
2026-03-18 19:22:02 -0300

    10.5. Adicionando Testcontainer aos testes de Presentation

--- src/test/java/com/algaworks/algashop/ordering/presentation/shoppincart/ShoppingCartControllerIT.java
+++ src/test/java/com/algaworks/algashop/ordering/presentation/shoppincart/ShoppingCartControllerIT.java
@@ -2,35 +2,23 @@ package com.algaworks.algashop.ordering.presentation.shoppincart;
 
 import com.algaworks.algashop.ordering.infrastructure.persistence.customer.CustomerPersistenceEntityRepository;
 import com.algaworks.algashop.ordering.infrastructure.persistence.shoppingcart.ShoppingCartPersistenceEntityRepository;
+import com.algaworks.algashop.ordering.presentation.AbstractPresentationIT;
 import com.algaworks.algashop.ordering.utils.AlgaShopResourceUtils;
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import io.restassured.RestAssured;
-import io.restassured.path.json.config.JsonPathConfig;
 import org.assertj.core.api.Assertions;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.util.UUID;
 
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-import static io.restassured.config.JsonConfig.jsonConfig;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql(scripts = "classpath:db/testdata/afterMigrate.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
-@Sql(scripts = "classpath:db/clean/afterMigrate.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_CLASS)
-public class ShoppingCartControllerIT {
-
-    @LocalServerPort
-    private int port;
+public class ShoppingCartControllerIT extends AbstractPresentationIT {
 
     @Autowired
     private CustomerPersistenceEntityRepository customerRepository;
@@ -41,38 +29,19 @@ public class ShoppingCartControllerIT {
     private static final UUID validCustomerId = UUID.fromString("6e148bd5-47f6-4022-b9da-07cfaa294f7a");
     private static final UUID validShoppingCartId = UUID.fromString("4f31582a-66e6-4601-a9d3-ff608c2d4461");
 
-    private WireMockServer wireMockProductCatalog;
-    private WireMockServer wireMockRapidex;
-
     @BeforeEach
     public void setup() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-        RestAssured.port = port;
-
-        RestAssured.config().jsonConfig(jsonConfig().numberReturnType(JsonPathConfig.NumberReturnType.BIG_DECIMAL));
-
-        initWireMock();
+        super.beforeEach();
     }
 
-    private void initWireMock() {
-        wireMockRapidex = new WireMockServer(options()
-                .port(8780)
-                .usingFilesUnderDirectory("src/test/resources/wiremock/rapidex")
-                .extensions(new ResponseTemplateTransformer(true)));
-
-        wireMockProductCatalog = new WireMockServer(options()
-                .port(8781)
-                .usingFilesUnderDirectory("src/test/resources/wiremock/product-catalog")
-                .extensions(new ResponseTemplateTransformer(true)));
-
-        wireMockRapidex.start();
-        wireMockProductCatalog.start();
+    @BeforeAll
+    static void setupBeforeAll() {
+        AbstractPresentationIT.initWireMock();
     }
 
-    @AfterEach
-    public void after() {
-        wireMockRapidex.stop();
-        wireMockProductCatalog.stop();
+    @AfterAll
+    static void after() {
+        AbstractPresentationIT.stopMock();
     }
 
     @Test
@@ -96,24 +65,5 @@ public class ShoppingCartControllerIT {
 
         Assertions.assertThat(shoppingCartRepository.existsById(createdShoppingCart)).isTrue();
     }
-
-//    @Test
-//    public void shouldAddProductToShoppingCart() {
-//        String json = AlgaShopResourceUtils.readContent("json/add-product-to-shopping-cart.json");
-//
-//        RestAssured
-//                .given()
-//                .accept(MediaType.APPLICATION_JSON_VALUE)
-//                .contentType(MediaType.APPLICATION_JSON_VALUE)
-//                .body(json)
-//                .when()
-//                .post("/api/v1/shopping-carts/{shoppingCartId}/items", validShoppingCartId)
-//                .then()
-//                .assertThat()
-//                .statusCode(HttpStatus.NO_CONTENT.value());
-//
-//        var shoppingCartPersistenceEntity = shoppingCartRepository.findById(validShoppingCartId).orElseThrow();
-//        Assertions.assertThat(shoppingCartPersistenceEntity.getTotalItems()).isEqualTo(4);
-//    }
 
 }
