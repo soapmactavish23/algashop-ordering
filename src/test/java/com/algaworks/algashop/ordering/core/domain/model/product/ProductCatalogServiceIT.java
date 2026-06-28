de4cd9591027885c3be32c44df1ad74d6a1fe14c
soapmactavish23 <ricknogueira1231@gmail.com>
2026-06-27 21:38:09 -0300

    Fix Data

--- src/test/java/com/algaworks/algashop/ordering/core/domain/model/product/ProductCatalogServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/core/domain/model/product/ProductCatalogServiceIT.java
@@ -1,46 +1,55 @@
 package com.algaworks.algashop.ordering.core.domain.model.product;
 
 import com.algaworks.algashop.ordering.infrastructure.adapters.out.web.product.client.http.ProductCatalogAPIClient;
+import com.algaworks.algashop.ordering.infrastructure.adapters.out.web.product.client.http.ProductResponse;
 import com.algaworks.algashop.ordering.utils.TestcontainerPostgreSQLConfig;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @Import(TestcontainerPostgreSQLConfig.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-class ProductCatalogServiceTest {
+class ProductCatalogServiceIT {
 
     @Autowired
     private ProductCatalogService productCatalogService;
 
-    @Autowired
+    @MockitoBean
     private ProductCatalogAPIClient productCatalogAPIClient;
 
     @Test
     void shouldHandleConcurrency() throws InterruptedException {
         UUID rawProductId = UUID.randomUUID();
         ProductId productId = new ProductId(rawProductId);
-        Mockito.when(productCatalogAPIClient.getById(rawProductId)).thenReturn(null);
+
+        ProductResponse response = new ProductResponse();
+        response.setId(rawProductId);
+        response.setName("Produto Teste");
+        response.setInStock(true);
+        response.setSalePrice(new BigDecimal("100.00"));
+
+        Mockito.when(productCatalogAPIClient.getById(rawProductId))
+                .thenReturn(response);
 
         try (ExecutorService executorService = Executors.newFixedThreadPool(10)) {
-            executorService.submit(() -> productCatalogService.ofId(productId));
-            executorService.submit(() -> productCatalogService.ofId(productId));
-            executorService.submit(() -> productCatalogService.ofId(productId));
-            executorService.submit(() -> productCatalogService.ofId(productId));
-            executorService.submit(() -> productCatalogService.ofId(productId));
-            executorService.submit(() -> productCatalogService.ofId(productId));
-            executorService.awaitTermination(30, TimeUnit.SECONDS);
-            executorService.shutdown();
-        }
-    }
+            for (int i = 0; i < 6; i++) {
+                executorService.submit(() -> productCatalogService.ofId(productId));
+            }
 
+            executorService.shutdown();
+            executorService.awaitTermination(30, TimeUnit.SECONDS);
+        }
+
+        Mockito.verify(productCatalogAPIClient, Mockito.times(6))
+                .getById(rawProductId);
+    }
 }
