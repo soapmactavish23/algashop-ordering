786f28bd3e0d04f580b9bbd4a2cc541cef456305
soapmactavish23 <ricknogueira1231@gmail.com>
2026-07-10 11:17:57 -0300

    5.2. Implementando gestão de Access Token para o RestClient.

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/web/product/client/http/ProductCatalogAPIConfig.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/web/product/client/http/ProductCatalogAPIConfig.java
@@ -1,10 +1,11 @@
 package com.algaworks.algashop.ordering.infrastructure.adapters.out.web.product.client.http;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -16,9 +17,14 @@ public class ProductCatalogAPIConfig {
 
     @Bean
     public ProductCatalogAPIClient productCatalogAPIClient(RestClient.Builder builder,
-                                                           @Value("${algashop.integrations.product-catalog.url}") String url) {
-        RestClient restClient = builder.baseUrl(url)
+                                                           ProductCatalogIntegrationProperties properties,
+                                                           OAuth2AuthorizedClientManager manager) {
+        var interceptor = new OAuth2ClientHttpRequestInterceptor(manager);
+        interceptor.setClientRegistrationIdResolver(_ -> properties.getOauth2ClientRegistrationId());
+
+        RestClient restClient = builder.baseUrl(properties.getUrl())
                 .requestFactory(generateClientHttpRequestFactory())
+                .requestInterceptor(interceptor)
                 .build();
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builderFor(adapter).build();
