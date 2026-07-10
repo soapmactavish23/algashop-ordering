1a7523d618a6ce3ce973a4c96a152bc622f75f3d
soapmactavish23 <ricknogueira1231@gmail.com>
2026-07-10 17:43:33 -0300

    5.3. Otimizando geração de token pelo Client

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/web/product/client/http/ProductCatalogAPIConfig.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/web/product/client/http/ProductCatalogAPIConfig.java
@@ -1,9 +1,12 @@
 package com.algaworks.algashop.ordering.infrastructure.adapters.out.web.product.client.http;
 
+import org.jspecify.annotations.Nullable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
@@ -11,6 +14,7 @@ import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.time.Duration;
+import java.util.Collections;
 
 @Configuration
 public class ProductCatalogAPIConfig {
@@ -21,6 +25,7 @@ public class ProductCatalogAPIConfig {
                                                            OAuth2AuthorizedClientManager manager) {
         var interceptor = new OAuth2ClientHttpRequestInterceptor(manager);
         interceptor.setClientRegistrationIdResolver(_ -> properties.getOauth2ClientRegistrationId());
+        interceptor.setPrincipalResolver(_ -> generatePrincipal(properties.getOauth2ClientRegistrationId()));
 
         RestClient restClient = builder.baseUrl(properties.getUrl())
                 .requestFactory(generateClientHttpRequestFactory())
@@ -29,6 +34,20 @@ public class ProductCatalogAPIConfig {
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builderFor(adapter).build();
         return proxyFactory.createClient(ProductCatalogAPIClient.class);
+    }
+
+    private Authentication generatePrincipal(String principalName) {
+        return new AbstractAuthenticationToken(Collections.emptySet()) {
+            @Override
+            public @Nullable Object getPrincipal() {
+                return principalName;
+            }
+
+            @Override
+            public @Nullable Object getCredentials() {
+                return null;
+            }
+        };
     }
 
     private ClientHttpRequestFactory generateClientHttpRequestFactory() {
